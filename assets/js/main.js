449cfb5ebcba3c3872455692756127c0abd15148
IgorKls <kls.18.igor@gmail.com>
2020-10-25 01:29:17 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let menuToggle = document.querySelector('#menu-toggle');
+let menu = document.querySelector('.sidebar');
+
+
+menuToggle.addEventListener('click', function (event) {
+
+    event.preventDefault();
+    menu.classList.toggle('visible');
+    menuToggle.classList.toggle('active');
+
+    // console.log(menuToggleSpan);
+
+    // menuToggleSpan[0].toggleAttribute.transform(rotate)
+
+})
